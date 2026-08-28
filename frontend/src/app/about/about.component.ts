1a2176acd5ebcf86d05aa5649c3a95078e90f6aa
Bartłomiej Wolski <bartlomiej.wolski@comarch.com>
2026-08-28 11:27:49 +0200

    feat: revamp about and login components with updated content and styling for Tibia portal

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
+  imports: [RouterLink],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
